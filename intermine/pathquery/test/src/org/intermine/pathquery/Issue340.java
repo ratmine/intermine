4eb89dd75a9e0473a930f16678069d6093e6b20a
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-06-06 10:45:39 +0100

    Added a stub of a regression test for #340

---
+++ intermine/pathquery/test/src/org/intermine/pathquery/Issue340.java
@@ -0,0 +1,9 @@
+package org.intermine.pathquery;
+
+/**
+ * Regression test to help fix and maintain the issue reported
+ * in https://github.com/intermine/intermine/issues/340
+ */
+public class Issue340 {
+}
+
