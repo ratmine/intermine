8b61061d7dd9ec92f1cac85c7660867d87d84196
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-06-06 17:43:45 +0100

    Added some actual testing logic

--- intermine/pathquery/test/src/org/intermine/pathquery/Issue340.java
+++ intermine/pathquery/test/src/org/intermine/pathquery/Issue340.java
@@ -1,9 +1,41 @@
 package org.intermine.pathquery;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.intermine.metadata.Model;
+
 /**
  * Regression test to help fix and maintain the issue reported
  * in https://github.com/intermine/intermine/issues/340
  */
 public class Issue340 {
+
+    private PathQuery query;
+
+    @Before
+    public void setup() {
+        Model model = Model.getInstanceByName("testmodel");
+        query = new PathQuery(model);
+        query.addViews(
+            "Employee.name",
+            "Employee.department.name",
+            "Employee.address.address",
+            "Employee.departmentThatRejectedMe.name",
+            "Employee.employmentPeriod"
+        );
+        query.setOuterJoinStatus("Employee.department", OuterJoinStatus.OUTER);
+        query.setOuterJoinStatus("Employee.address", OuterJoinStatus.OUTER);
+        query.setOuterJoinStatus("Employee.departmentThatRejectedMe", OuterJoinStatus.OUTER);
+        query.setOuterJoinStatus("Employee.employmentPeriod", OuterJoinStatus.OUTER);
+
+    }
+
+    @Test
+    public void isValid() {
+        assertTrue(query.isValid());
+    }
 }
 
